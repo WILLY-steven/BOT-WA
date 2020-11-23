817fcb9c50d017a8a1a93cd9a8bf1ec6dad8a2f2
WILLY-steven <64624349+WILLY-steven@users.noreply.github.com>
2020-11-24 00:11:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'NAMA BOTMU'; 
-const instagram = 'LINK INSTAGRAMU'; 
-const telegram = 'LINK TELEMU'; 
+const BotName = 'WILLY-BOT'; 
+const instagram = 'https://instagram.com/Willy_febriano.pasaribu'; 
+const telegram = 'https://t.me/Willy_pasaribu'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'LINK YTMU';
 //const grupch1 = 'belum ada grup'; 
@@ -64,7 +64,7 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@reuz.id')
+conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@Willy_febriano.pasaribu')
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
